6c35b46ab9248b3e821dc913dd8e4cfb049075b4
Galen Gidman <galen@galengidman.com>
2014-10-08 10:50:03 -0500

    cleaned up body classes

--- includes/body-classes.php
+++ includes/body-classes.php
@@ -8,72 +8,59 @@
  */
 function restful_body_classes( $classes ) {
 
-  // Blog
-  if (
-    is_home()
-    ||
-    is_category()
-    ||
-    is_tag()
-    ||
-    is_author()
-    ||
-    is_date()
-    ||
-    is_singular( 'post' )
-  ) $classes[] = 'restful-blog';
-
   // Event
-  if ( is_singular( 'ctc_event' ) ) $classes[] = 'restful-event';
+  if ( is_singular( 'ctc_event' ) ) {
+    $classes[] = 'restful-event';
+  }
 
   // Events
-  if (
-    is_page_template( 'page-templates/events.php' )
-    ||
-    is_post_type_archive( 'ctc_event' )
-  ) $classes[] = 'restful-events';
+  if ( is_post_type_archive( 'ctc_event' ) ) {
+    $classes[] = 'restful-events';
+  }
 
   // Locations
-  if (
-    is_page_template( 'page-templates/locations.php' )
-    ||
-    is_post_type_archive( 'ctc_location' )
-    ||
-    is_singular( 'ctc_location' )
-  ) $classes[] = 'restful-locations';
+  if ( is_post_type_archive( 'ctc_location' ) ) {
+    $classes[] = 'restful-locations';
+  }
 
   // Location
-  if ( is_singular( 'ctc_location' ) ) $classes[] = 'restful-location';
+  if ( is_singular( 'ctc_location' ) ) {
+    $classes[] = 'restful-location';
+  }
 
   // People
-  if (
-    is_page_template( 'page-templates/people.php' )
-    ||
-    is_post_type_archive( 'ctc_person' )
-    ||
-    is_tax( 'ctc_person_group' )
-  ) $classes[] = 'restful-people';
+  if ( is_post_type_archive( 'ctc_person' ) || is_tax( 'ctc_person_group' ) ) {
+    $classes[] = 'restful-people';
+  }
 
   // Person
-  if ( is_singular( 'ctc_person' ) ) $classes[] = 'restful-person';
+  if ( is_singular( 'ctc_person' ) ) {
+    $classes[] = 'restful-person';
+  }
 
   // Sermons
-  if (
-    is_page_template( 'page-templates/sermons.php' )
-    ||
-    is_post_type_archive( 'ctc_sermon' )
-  ) $classes[] = 'restful-sermons';
+  $sermon_taxonomies = array(
+    'ctc_sermon_book',
+    'ctc_sermon_series',
+    'ctc_sermon-series',
+    'ctc_sermon_tag',
+    'ctc_sermon_topic'
+  );
+
+  if ( is_post_type_archive( 'ctc_sermon' ) || is_tax( $sermon_taxonomies ) ) {
+    $classes[] = 'restful-sermons';
+  }
 
   // Sermon
-  if ( is_singular( 'ctc_sermon' ) ) $classes[] = 'restful-sermon';
-
-  // No sidebar page template
-  if ( is_page_template( 'page-templates/no-sidebar.php' ) ) $classes[] = 'restful-no-sidebar';
+  if ( is_singular( 'ctc_sermon' ) ) {
+    $classes[] = 'restful-sermon';
+  }
 
   // Home page template
-  if ( is_page_template( 'page-templates/home.php' ) ) $classes[] = 'restful-home';
+  if ( is_page_template( 'page-templates/home.php' ) ) {
+    $classes[] = 'restful-home';
+  }
 
-  // Return the custom classes
   return $classes;
 
 }
