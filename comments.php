02f3556f2d8fe6c4d99db7b63a47cd3ac4a77808
Galen Gidman <galen@galengidman.com>
2014-10-10 15:40:01 -0500

    comments fixes and checks - closes #4 #5

--- comments.php
+++ comments.php
@@ -1,19 +1,38 @@
-<?php if ( comments_open() ) : ?>
+<?php
+
+if ( post_password_required() ) {
+  return;
+}
+
+if ( comments_open() ) : ?>
   <div class="comments-wrapper">
-    <div class="replies-wrapper">
-      <h3><?php comments_number(); ?></h3>
 
-      <ol class="comments-list">
-        <?php wp_list_comments(); ?>
-      </ol>
+    <?php if ( have_comments() ) : ?>
+      <div class="replies-wrapper">
+        <h3><?php comments_number(); ?></h3>
 
-      <?php if ( get_previous_comments_link() || get_next_comments_link() ) : ?>
-        <div class="pagination">
-          <?php paginate_comments_links(); ?>
-        </div>
-      <?php endif; ?>
-    </div>
+        <ol class="comments-list">
+          <?php
+
+          $args = array(
+            'style'       => 'ol',
+            'short_ping'  => true,
+            'avatar_size' => 60
+          );
+
+          wp_list_comments( $args );
+
+          ?>
+        </ol>
+
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+          <div class="pagination">
+            <?php paginate_comments_links(); ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
 
     <?php restful_comment_form(); ?>
   </div>
-<?php endif; ?>
+<?php endif;
