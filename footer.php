ab582ad96c8316df108bad817cd1b73eaaaf0204
Galen Gidman <galen@galengidman.com>
2014-10-11 13:23:44 -0500

    replaced ThemeBright logo with SVG from framework

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
             &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>
           </div>
 
-          <div class="theme-credit col col-xs-12 col-md-6"><a href="http://themebright.co/" class="themebright-logo"><?php locate_template( 'assets/img/themebright-logo.svg', 'true' ); ?></a></div>
+          <div class="theme-credit col col-xs-12 col-md-6"><a href="http://themebright.co/" class="themebright-logo"><?php locate_template( TBF_DIR . '/assets/img/themebright-logotype.svg', 'true' ); ?></a></div>
         </div>
       </div>
     </footer>
