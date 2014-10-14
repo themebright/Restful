8281704349cbce7c1008676f8b4e1bacbcae4663
Galen Gidman <galen@galengidman.com>
2014-10-14 13:44:13 -0500

    updated function name

--- includes/scripts.php
+++ includes/scripts.php
@@ -10,8 +10,8 @@ function restful_add_scripts() {
 
   if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 
-  wp_enqueue_script( 'restful-bxslider', tbf_template_url() . '/assets/js/library/jquery.bxslider.js', array( 'jquery' ), TBF_THEME_VERSION );
-  wp_enqueue_script( 'restful', tbf_template_url() . '/assets/js/restful.js', array( 'jquery', 'restful-bxslider' ), TBF_THEME_VERSION );
+  wp_enqueue_script( 'restful-bxslider', tbf_get_template_directory_uri() . '/assets/js/library/jquery.bxslider.js', array( 'jquery' ), TBF_THEME_VERSION );
+  wp_enqueue_script( 'restful', tbf_get_template_directory_uri() . '/assets/js/restful.js', array( 'jquery', 'restful-bxslider' ), TBF_THEME_VERSION );
 
 }
 add_action( 'wp_enqueue_scripts', 'restful_add_scripts' );
