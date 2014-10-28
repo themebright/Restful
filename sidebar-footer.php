21fa1322ab7832c059e0a5c0adf328c6b03ea6c5
Galen Gidman <galen@galengidman.com>
2014-10-28 13:08:19 -0500

    some sidebar style updates

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -4,19 +4,19 @@
   <div class="container">
     <div class="row">
       <?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-        <div class="widget-area col col-xs-12 col-md-4">
+        <div class="widget-area col col-xs-12 col-md-3">
           <?php dynamic_sidebar( 'footer-1' ); ?>
         </div>
       <?php endif; ?>
 
       <?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
-        <div class="widget-area col col-xs-12 col-md-4">
+        <div class="widget-area col col-xs-12 col-md-3">
           <?php dynamic_sidebar( 'footer-2' ); ?>
         </div>
       <?php endif; ?>
 
       <?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
-        <div class="widget-area col col-xs-12 col-md-4">
+        <div class="widget-area col col-xs-12 col-md-3">
           <?php dynamic_sidebar( 'footer-3' ); ?>
         </div>
       <?php endif; ?>
