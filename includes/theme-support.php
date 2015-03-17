0e231439648db5ab9820148c64db026051df4bee
Galen Gidman <galen@galengidman.com>
2015-03-17 12:04:34 -0500

    add_theme_support( 'title-tag' )

--- includes/theme-support.php
+++ includes/theme-support.php
@@ -10,7 +10,7 @@ function restful_add_theme_support() {
 
   add_theme_support( 'automatic-feed-links' );
 
-  add_theme_support( 'post-thumbnails' );
+  add_theme_support( 'brightslider' );
 
   add_theme_support( 'html5', array(
     'comment-list',
@@ -20,13 +20,15 @@ function restful_add_theme_support() {
     'caption'
   ) );
 
+  add_theme_support( 'jetpack-responsive-videos' );
+
+  add_theme_support( 'post-thumbnails' );
+
   add_theme_support( 'site-logo', array(
     'size' => 'large'
   ) );
 
-  add_theme_support( 'jetpack-responsive-videos' );
-
-  add_theme_support( 'brightslider' );
+  add_theme_support( 'title-tag' );
 
 }
 add_action( 'after_setup_theme', 'restful_add_theme_support' );
