ce16decf93955eba208eed2667202cdd441f9d6f
Galen Gidman <galen@galengidman.com>
2015-03-18 18:16:16 -0500

    widget updates

--- includes/tbf.php
+++ includes/tbf.php
@@ -8,52 +8,21 @@
  */
 function restful_tbf_support() {
 
-  add_theme_support( 'tbf-widget-events', array(
-    'options' => array(
-      'title'     => true,
-      'number'    => true,
-      'thumbnail' => true,
-      'excerpt'   => false,
-      'date'      => true,
-      'time'      => true,
-      'venue'     => false,
-      'address'   => false,
-      'map'       => false
-    )
-  ) );
 
-  add_theme_support( 'tbf-widget-locations', array(
-    'options' => array(
-      'title'     => true,
-      'thumbnail' => true,
-      'excerpt'   => false,
-      'address'   => true,
-      'phone'     => false,
-      'times'     => true
-    )
-  ) );
-
-  add_theme_support( 'tbf-widget-people', array(
-    'options' => array(
-      'title'     => true,
-      'group'     => true,
-      'thumbnail' => true,
-      'excerpt'   => true,
-      'position'  => true,
-      'phone'     => false,
-      'email'     => false,
-      'urls'      => false
-    )
-  ) );
-
-  add_theme_support( 'tbf-widget-sermons', array(
-    'options' => array(
-      'title'     => true,
-      'number'    => true,
-      'thumbnail' => true,
-      'excerpt'   => true,
-      'date'      => true,
-      'media'     => false
+  add_theme_support( 'tbf', array(
+    'widgets' => array(
+      'events' => array(
+        'fields' => array( 'title', 'number', 'thumbnail', 'date', 'time' )
+      ),
+      'locations' => array(
+        'fields' => array( 'title', 'thumbnail', 'address', 'times', 'map' )
+      ),
+      'people' => array(
+        'fields' => array( 'title', 'group', 'number', 'thumbnail', 'excerpt', 'position' )
+      ),
+      'sermons' => array(
+        'fields' => array( 'title', 'number', 'thumbnail', 'excerpt', 'date' )
+      )
     )
   ) );
 
