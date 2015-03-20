c726b1b1156737d056f24b87c3368eb1b67999ae
Galen Gidman <galen@galengidman.com>
2015-03-20 10:52:20 -0500

    starting to work on comment styling

--- comments.php
+++ comments.php
@@ -2,35 +2,46 @@
 
 if ( post_password_required() ) return;
 
-if ( comments_open() ) : ?>
-  <div class="comments">
+?>
 
-    <?php if ( have_comments() ) : ?>
-      <div class="comments__replies">
-        <h3><?php comments_number(); ?></h3>
+<div id="comments" class="comments">
+  <?php if ( have_comments() ) : ?>
+    <div class="comments__replies">
+      <h2 class="comments__replies-title"><?php comments_number(); ?></h2>
+    </div>
 
-        <ol class="comments__replies-list">
-          <?php
+    <ol class="comments__list">
+      <?php
 
-          $args = array(
-            'style'       => 'ol',
-            'short_ping'  => true,
-            'avatar_size' => 60
-          );
+      $args = array(
+        'style'       => 'ol',
+        'short_ping'  => true,
+        'avatar_size' => 60
+      );
 
-          wp_list_comments( $args );
+      wp_list_comments( $args );
 
-          ?>
-        </ol>
+      ?>
+    </ol>
 
-        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-          <div class="pagination">
-            <?php paginate_comments_links(); ?>
-          </div>
-        <?php endif; ?>
-      </div>
+    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+      <nav class="comments__pagination pagination">
+        <?php paginate_comments_links(); ?>
+      </nav>
     <?php endif; ?>
 
-    <?php restful_comment_form(); ?>
-  </div>
-<?php endif;
+    <?php if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+      <div class="comments__closed alert alert--error"><?php _e( 'Comments are closed.', 'restful' ); ?></div>
+    <?php endif; ?>
+
+    <?php
+
+    $args = array(
+      'comment_notes_after' => false
+    );
+
+    comment_form( $args );
+
+    ?>
+  <?php endif; ?>
+</div>
--- includes/template-tags.php
+++ includes/template-tags.php
@@ -48,28 +48,6 @@ function restful_show_posts_nav() {
 }
 
 /**
- * Wrap and configures comment_form() for custom display.
- */
-function restful_comment_form() {
-
-  $fields = array(
-    'author' => '<div class="row author-info"><div class="col col--xs--12 col--sm--6"><input class="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" placeholder="' . __( 'Your Name', 'restful' ) . ' *"></div>',
-    'email'  => '<div class="col col--xs--12 col--sm--6"><input class="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" placeholder="' . __( 'Your Email', 'restful' ) . ' *"></div></div>',
-    'url'    => ''
-  );
-
-  $args = array(
-    'comment_field'        => '<textarea class="comment" name="comment" rows="10" placeholder="' . __( 'Your Comment', 'restful' ) . ' *"></textarea>',
-    'fields'               => $fields,
-    'comment_notes_before' => '',
-    'comment_notes_after'  => ''
-  );
-
-  comment_form( $args );
-
-}
-
-/**
  * Configures and displays wp_link_pages() to fit in with theme styling.
  */
 function restful_wp_link_pages( $content ) {
