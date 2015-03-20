9b6cdab3c61c5b309f060a336ed9acf8edc2b15f
Galen Gidman <galen@galengidman.com>
2015-03-19 21:36:09 -0500

    escape urls and add wp_link_pages to the_content via filter

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -14,12 +14,12 @@ function restful_post_meta() {
 
   <div class="entry__meta-item">
     <i class="fa fa-calendar"></i>
-    <a href="<?php the_permalink(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
+    <a href="<?php esc_url( get_permalink() ); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
   </div>
 
   <div class="entry__meta-item">
     <i class="fa fa-user"></i>
-    <a href="<?php echo get_author_posts_url( $post->post_author ); ?>"><?php the_author_meta( 'display_name', $post->post_author ); ?></a>
+    <a href="<?php echo esc_url( get_author_posts_url( $post->post_author ) ); ?>"><?php the_author_meta( 'display_name', $post->post_author ); ?></a>
   </div>
 
   <div class="entry__meta-item">
@@ -29,7 +29,7 @@ function restful_post_meta() {
 
   <div class="entry__meta-item">
     <i class="fa fa-comment"></i>
-    <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
+    <a href="<?php esc_url( comments_link() ); ?>"><?php comments_number(); ?></a>
   </div>
 
 <?php
@@ -72,15 +72,17 @@ function restful_comment_form() {
 /**
  * Configures and displays wp_link_pages() to fit in with theme styling.
  */
-function restful_link_pages() {
+function restful_wp_link_pages( $content ) {
 
-  $args = array(
+  $wp_link_pages_args = array(
     'before'      => '<nav class="pagination">',
     'after'       => '</nav>',
     'link_before' => '<span class="page-numbers">',
-    'link_after'  => '</span>'
+    'link_after'  => '</span>',
+    'echo'        => false
   );
 
-  wp_link_pages( $args );
+  return $content . wp_link_pages( $wp_link_pages_args );
 
 }
+add_filter( 'the_content', 'restful_wp_link_pages' );
